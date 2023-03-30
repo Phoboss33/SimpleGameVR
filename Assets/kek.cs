a6e9824274fbe41cab5236298a73eb8155c2f9d6
Phoboss <mavirtex@gmail.com>
2023-03-30 15:06:26 +0500

    fix

---
+++ Assets/kek.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kek : MonoBehaviour
+{
+    public GameObject spheressssss;
+    public Transform cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 currentPosition = transform.position;
+        //gameObject.SetActive(false);
+        spheressssss.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
